5f55136b71d0edaed52a0d8d617e7c5b6235c845
KevinLin13 <95026554+KevinLin13@users.noreply.github.com>
2026-09-20 17:05:50 +0800

    Complete Employee Data SQL challenges

---
+++ employee-data/solutions/basic/03_employees_over_30.sql
@@ -0,0 +1,11 @@
+-- Challenge:
+-- List the names and ages of employees older than 30.
+
+-- Concepts:
+-- SELECT, WHERE, comparison operator
+
+SELECT
+    ename,
+    age
+FROM employee
+WHERE age > 30;
